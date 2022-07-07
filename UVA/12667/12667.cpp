6a54c7e75dc0c2980584508c21c267c231e5aecc
Pimyn <pimyn@girgis.org>
2022-07-07 23:59:15 +0200

    commit

--- UVA/12667/12667.cpp
+++ UVA/12667/12667.cpp
@@ -8,18 +8,20 @@ int lastID[15];
 
 int main()
 {
-	int n, t, m, time, id, prob;
-	char in[5];
+	int n, t, m, time, id;
+	char in[5], prob;
 	scanf("%d %d %d", &n, &t, &m);
+
 	for(int i=0;i<m;++i)
 	{
-		scanf("%d %d %d %s", &time, &id, &prob, in);
+		scanf("%d %d %c %s", &time, &id, &prob, in);
 		prob -= 'A';
 		if(in[0] == 'Y' && !solved[id][prob])
 			solved[id][prob] = 1, lastID[prob] = id, lastTime[prob] = time, done[prob] = 1;
 	}
+
 	for(int i=0;i<n;++i)
 		if(done[i]) printf("%c %d %d\n", i + 'A', lastTime[i], lastID[i]);
-		else printf("%c - -", i + 'A');
+		else printf("%c - -\n", i + 'A');
 	return 0;
 }
