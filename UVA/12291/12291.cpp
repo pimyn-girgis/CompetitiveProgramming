64ec34072a63922d53e94b321312a0a9d06ff592
Pimyn Girgis <bemenboshra2001@gmail.com>
2022-07-13 13:31:15 +0200

    Update 12291.cpp

--- UVA/12291/12291.cpp
+++ UVA/12291/12291.cpp
@@ -1,85 +1,89 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool large[10][10];
-bool small[10][10];
-bool composed[21][21];
-bool test[21][21];
+bool sml[10][10], lrg[10][10], comp[10][10];
 int m, n;
 
-bool translate(const int dim, const int x, const int y, bool dest[][21], bool src[][10])
+void init()
 {
-	for (int i = 0; i < dim; ++i)
-		for (int j = 0; j < dim; ++j){
-			if (!src[i][j]) continue;
-			auto & c = dest[i+x][j+y];
-			if (c) return false;
-			c = 1;
-		}
-	return true;
+	memset(sml, 0, sizeof(sml));
+	memset(lrg, 0, sizeof(lrg));
 }
 
-void print(bool a[][22]) {
-	for (int i = 0; i < 22; ++i)
-	for (int j = 0; j < 22; ++j)
-	printf("%c%s", a[i][j] ? '1' : '0', j == 21 ? "\n" : "");
-}
-
-bool match() {
-	for (int x = 0; x < m; ++x)
-	for (int y = 0; y < m; ++y)
-	{
-		memset(test, 0, sizeof(test));
-		translate(n, x, y, test, large);
-		if (!memcmp(test, composed, sizeof(test))) return true;
-	}
-	return false;
-}
-bool solve()
+void fix()
 {
-	for (int u = 0; u <= n; ++u)
-	for (int v = 0; v <= n; ++v)
-	for (int x = 0; x <= n; x++)
-	for (int y = 0; y <= n; y++)
+	for(int j=0;j<m;++j)
 	{
-		// Place a copy of the small shape with the bottom left corner at (0, 0)
-		// and another copy with the bottom left corner at (x, y),
-		// and see if this gives you the large shape.
-		memset(composed, 0, sizeof(composed));
-		translate(m, u, v, composed, small);
-		if (!translate(m, x, y, composed, small)) continue;
-		if (match()) return 1;
+		for(int i=0;i<m;++i)
+			if(sml[i][0]) goto label;
+
+			for(int a=0;a<m;++a)
+				for(int b=0;b<m-1;++b)
+					swap(sml[a][b] ,sml[a][b+1]);
 	}
-	return 0;
+label:
+	for(int j=0;j<m;++j)
+	{
+		for(int i=0;i<m;++i)
+			if(sml[0][i]) return;
+
+			for(int a=0;a<m-1;++a)
+				for(int b=0;b<m;++b)
+					swap(sml[a][b] ,sml[a+1][b]);
+	}
 }
 
 bool input()
 {
-	int l = 0;
-	char input[11];
+	int invalid = 0;
+	char in[15];
 	for(int i=0;i<n;++i)
-	{
-		scanf("%s", input);
-		for(int j=0;j<n;++j)
-			l += (large[i][j] = input[j] == '*');
-	}
-
-	int s = 0;
-	for(int i=0;i<m;++i)
-	{
-		scanf("%s", input);
-		for(int j=0;j<m;++j)
-			s += (small[i][j] = input[j] == '*');
-	}
-
-	return s * 2 == l;
+		{
+			scanf("%s", in);
+			for(int j=0;in[j];++j)
+			invalid += (lrg[i][j] = in[j] == '*');
+		}
+		for(int i=0;i<m;++i)
+		{
+			scanf("%s", in);
+			for(int j=0;in[j];++j)
+			invalid -= 2 * (sml[i][j] = in[j] == '*');
+		}
+	return !invalid;
 }
 
-void init()
+bool valid(int x){return x < n && x >= 0;}
+bool valid(int x, int y){return valid(x) && valid(y);}
+
+bool Set(int a, int b)
 {
-	memset(large, 0, sizeof(large));
-	memset(small, 0, sizeof(small));
-	memset(composed, 0, sizeof(composed));
+	for(int x=a;x<a+m;++x)
+		for(int y=b;y<b+m;++y)
+			if(valid(x, y) && sml[x-a][y-b]) 
+			{
+				if(comp[x][y]) return 0;
+				comp[x][y] = 1;
+			}
+	return 1;
+}
+
+bool test(int i, int j, int a, int b)
+{
+	memset(comp, 0, sizeof(comp));
+	if(!(Set(i, j) && Set(a, b))) return 0;
+	return (!memcmp(lrg, comp, sizeof(lrg)));
+}
+
+bool solve()
+{
+	fix();
+	for(int i=0;i<n;++i)
+		for(int j=0;j<n;++j)
+			for(int a=0;a<n;++a)
+				for(int b=0;b<n;++b)
+					if(test(i, j, a, b))
+						return 1;
+	return 0;
 }
 
 int main()
@@ -87,7 +91,7 @@ int main()
 	while(scanf("%d %d", &n, &m) && (m || n))
 	{
 		init();
-		printf("%d\n", input() && solve());
+		printf("%d\n", (input() && solve()));
 	}
 	return 0;
 }
