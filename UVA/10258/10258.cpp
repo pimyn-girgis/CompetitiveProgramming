2d921c3073fb3a46c140aa97fad5a84e3b0a6d73
pimyn-girgis <bemenboshra2001@gmail.com>
2022-07-18 14:41:37 +0200

    10258 (input not done)

--- UVA/10258/10258.cpp
+++ UVA/10258/10258.cpp
@@ -1,47 +1,72 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define rank tuple<int, int, int> //solved, penalty, number
+int score[105];
+int pen[105];
+int potPen[105][9];
+bool solved[105][9];
+bool sub[105];
+char in[100];
+int Time, con, prob;
+char judge;
+int leader[105];
 
-vector <rank> board(105);
-
-bool comp(rank fir, rank sec)
+void init()
 {
-	auto &sol1 = get<0>(fir), &pen1 = get<1>(fir), &num1 = get<2>(fir);
-	auto &sol2 = get<0>(sec), &pen2 = get<1>(sec), &num2 = get<2>(sec);
-
-	if(sol1 != sol2)
-		return sol1 > sol2;
-	else if(pen1 != pen2)
-		return pen1 < pen2;
-	else
-		return num1 < num2;
+	memset(score, 0, sizeof(score));
+	memset(solved, 0, sizeof(solved));
+	memset(pen, 0, sizeof(pen));
+	memset(potPen, 0, sizeof(potPen));
+	memset(sub, 0, sizeof(sub));
+	for(int i=0;i<105;++i) leader[i] = i;
 }
 
-void enter(int num, int prob, int tim, char judge)
+bool comp(int x, int y)
 {
-	board[num];
-	auto &sol1 = get<0>(fir), &pen1 = get<1>(fir), &num1 = get<2>(fir);
-	auto &sol2 = get<0>(sec), &pen2 = get<1>(sec), &num2 = get<2>(sec);
+	if(score[x] != score[y])
+		return score[x] > score[y];
+	if(pen[x] != pen[y])
+		return pen[x] < pen[y];
+	return x < y;
 }
 
+void output()
+{
+
+}
 int main()
 {
 	int ts;
-	char in[105];
-	int time, prob, num;
-	char judge;
-
 	scanf("%d", &ts);
-	while(ts--)
+	fgets(in, 100, stdin);
+	while (ts--)
 	{
-		fgets(in, 100, stdin);
-		if(!strcmp(in, "\n")) solve();
-		else
+		init();
+		while(1)
 		{
-			sscanf(in, "%d %d %d %c", &num, &prob, &tim, &judge);
-			enter(num, prob, tim, judge);
+			fgets(in, 100, stdin);
+			if(feof(stdin) || !strcmp(in, "\n")) output();
+			sscanf(in, "%d %d %d %c", &con, &prob, &Time, &judge);
+			--con, --prob;
+			sub[con] = 1;
+
+			if(!solved[con][prob])
+			{
+				if(judge == 'I') potPen[con][prob] += 20;
+				else if(judge == 'C')
+				{
+					solved[con][prob] = 1;
+					pen[con] += potPen[con][prob] + Time;
+					++score[con];
+				}
+			}
 		}
+		stable_sort(leader, leader+105, comp);
+		for(int i=0;i<105;++i)
+		{
+			int index = leader[i];
+			if(sub[index]) printf("%d %d %d\n", index+1, score[index], pen[index]);
+		}
+		if(ts) printf("\n");
 	}
-	return 0;
 }
