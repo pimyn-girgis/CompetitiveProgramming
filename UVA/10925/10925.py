a58f914f0b054b4f3fe5e67078666a75a6970271
pimyn-girgis <bemenboshra2001@gmail.com>
2022-07-25 01:40:11 +0200

    solved some big integer problems

--- UVA/10925/10925.py
+++ UVA/10925/10925.py
@@ -2,14 +2,14 @@ caseNo = 1
 while True:
     n, f = [int(x) for x in input().split()]
 
-    if n == 0 and f == 0:
+    if not(n or f):
         break
     
     sum = 0
-    for i in range(int(n)):
+    for i in range(n):
         sum = int(input()) + sum
     
     print('Bill #%d costs %d: each friend should pay %d\n'
         %(caseNo, sum, sum // f))
 
-    caseNo = caseNo + 1
+    caseNo += 1
