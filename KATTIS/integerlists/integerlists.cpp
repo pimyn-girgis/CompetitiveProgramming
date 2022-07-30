7e83834aa57577318ffd16074cc6d548f7cf10db
pimyn-girgis <bemenboshra2001@gmail.com>
2022-07-30 23:14:22 +0200

    Fixed KATTIS/integerlists

--- KATTIS/integerlists/integerlists.cpp
+++ KATTIS/integerlists/integerlists.cpp
@@ -1,4 +1,4 @@
-//https://open.kattis.com/problems/PROBLEM_NAME
+//https://open.kattis.com/problems/integerlists
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -6,59 +6,59 @@ char cmd[int(1e6 + 5)];
 
 int main() 
 {
-	int tc, num, val;
-	scanf("%d", &tc);
-	list <int> ints;
-	while(tc--)
-	{
-		ints.clear();
-		bool error = 0, back = 0;
+    int tc, num, val;
+    scanf("%d ", &tc);
+    list <int> ints;
+    while(tc--)
+    {
+        ints.clear();
+        bool error = 0, back = 0;
 
-		scanf(" %s %d ", cmd, &num);
+        scanf("%s %d ", cmd, &num);
 
-		if(!num)
-			scanf("[");
-		while(num--)
-			scanf("%*c%d", &val), ints.push_back(val);
-		scanf("]");
+        if(!num)
+            scanf("[");
+        while(num--)
+            scanf("%*c%d", &val), ints.push_back(val);
+        scanf("] ");
 
-		for(int i = 0;cmd[i]; ++i)
-		{
-			char c = cmd[i];
-			if(c == 'R')
-				back = !back;
-			else if(c == 'D')
-			{
-				if(ints.empty())
-				{
-					error = 1;
-					break;
-				}
+        for(int i = 0;cmd[i]; ++i)
+        {
+            char c = cmd[i];
+            if(c == 'R')
+                back = !back;
+            else if(c == 'D')
+            {
+                if(ints.empty())
+                {
+                    error = 1;
+                    break;
+                }
 
-				if(back) ints.pop_back();
-				else ints.pop_front();
-			}
-		}
+                if(back) ints.pop_back();
+                else ints.pop_front();
+            }
+        }
 
-		if(error)
-			printf("error\n");
-		else
-		{
-			printf("[");
+        if(error)
+            printf("error\n");
+        else
+        {
+            printf("[");
 
-			if(ints.empty())
-				printf("]");
+            if(ints.empty())
+                printf("]");
 
-			else if(back)
-				while(!ints.empty())
-					printf("%d%c", ints.back(), ints.back() == ints.front()? ']' : ','), ints.pop_back();
+            else if(back)
+                while(!ints.empty())
+                    printf("%d%c", ints.back(), &ints.back() == &ints.front()? ']' : ','), ints.pop_back();
 
-			else
-				while(!ints.empty())
-					printf("%d%c", ints.front(), ints.front() == ints.back()? ']' : ','), ints.pop_front();
+            else
+                while(!ints.empty())
+                    printf("%d%c", ints.front(), &ints.front() == &ints.back()? ']' : ','), ints.pop_front();
 
-			printf("\n");
-		}
-	}
-	return 0;
+            printf("\n");
+        }
+    }
+    return 0;
 }
