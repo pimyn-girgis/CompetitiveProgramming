f441c16cd83870d590737443d3cef523f8e011a1
pimyn-girgis <bemenboshra2001@gmail.com>
2022-08-04 15:30:02 +0200

    solved 11997

--- UVA/11997/11997.cpp
+++ UVA/11997/11997.cpp
@@ -1,31 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int sum[int(1e6)];
-int arr[755][755];
+#define MAX 755
+int arr1[MAX], arr2[MAX], sum[MAX * MAX];
 int k;
-
-void makeSet(int index, int setNum)
-{
-	if(index == k - 1) return;
-
-	for(int i = 0; i < k; ++i)
-	{
-		sum[index] += arr[ ]
-	}
-}
 
 int main()
 {
 	while (scanf("%d", &k) != EOF)
 	{
-		memset(sum, 0, k * k * sizeof(int));
+		for(int i = 0; i < k; ++i)
+			scanf("%d", sum + i);
+
+		for(int m = 1; m < k; ++m)
+		{
+			memcpy(arr1, sum, k * sizeof(int));
+
+			for(int i = 0; i < k; ++i)
+				scanf("%d", arr2 + i);
+
+			for(int i = 0, index = 0; i < k; ++i)
+				for(int j = 0; j < k; ++j, ++index)
+					sum[index] = arr1[i] + arr2[j];
+
+			partial_sort(sum, sum + k, sum + k * k);
+		}
 
 		for(int i = 0; i < k; ++i)
-			for(int j = 0; j < k; ++j)
-				scanf("%d", arr[i] + j);
-
-		calc(0, 0);
+			printf("%d%c", sum[i], i == k - 1? '\n' : ' ');
 	}
 	return 0;
 }
