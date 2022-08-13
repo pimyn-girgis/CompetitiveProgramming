31733aaa53f878535c78c3d37365cf97eba9d53c
pimyn-girgis <bemenboshra2001@gmail.com>
2022-08-13 17:44:15 +0200

    solved 13037 and added Book/

--- test.cpp
+++ test.cpp
@@ -3,32 +3,8 @@ using namespace std;
 
 int main()
 {
-	int n, s;
-	scanf("%d %d", &n, &s);
-	vector <tuple<long double, int, int> > v(n);
-
-	for(int i = 0; i < n; ++i)
-	{
-		auto &[weight_for_cost, cost, weight] = v[i];
-		scanf("%d %d", &weight, &cost);
-		weight_for_cost = float(weight)/cost;
-	}
-
-	int sum = 0;
-	sort(v.begin(), v.end());
-
-	auto it = v.begin();
-	int totalCost = 0;
-	while(sum < s && it != v.end())
-	{
-		auto &[weight_for_cost, cost, weight] = *it;
-		if(weight + sum < s)
-			sum += weight, totalCost += cost;
-		else
-			++it;
-	}
-
-	printf("%d\n", totalCost);
-
+	freopen("in", "w", stdout);
+	for(int i = 1; i <= 300000; ++i)
+		printf("%d\n", i);
 	return 0;
 }
