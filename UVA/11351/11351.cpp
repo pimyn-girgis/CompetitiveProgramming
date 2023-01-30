6632d2b2fd120ed858d640ed8ca31203d4a2dc58
pimyn-girgis <bemenboshra2001@gmail.com>
2023-01-30 20:44:52 +0200

    Solved uva

--- UVA/11351/11351.cpp
+++ UVA/11351/11351.cpp
@@ -2,35 +2,26 @@
 **************************************************************
 * Author  : Bemen Girgis
 * Contact : bemen@girgis.org
-* Problem : ##### UVA
-* Link    : https://onlinejudge.org/external/###/p#####.pdf
+* Problem : 11351 UVA
+* Link    : https://onlinejudge.org/external/113/p11351.pdf
 **************************************************************
 * Approach: 
 **************************************************************
 */
 
 #include <cstdio>
-#include <cmath>
 using namespace std;
 
-int solve(int &n) {
-	int shift = 31 - __builtin_clz(n);
-	return ((n & ~(1 << shift)) << 1) | 1;
+int solve(int n, int k) {
+	return n == 1? 0 : (k + solve(n - 1, k)) % n;
 }
 
 int main() {
 	int tc; scanf("%d", &tc);
 
 	for (int no = 1; no <= tc; ++no) {
-		int n; scanf("%d", &n);
-		int rep = -1;
-
-		for (int survivor = -1; survivor != n; n = solve(n)) {
-			survivor = n;
-			++rep;
-		}
-
-		printf("Case %d: %d %d\n", no, rep, n);
+		int n, k; scanf("%d %d", &n, &k);
+		printf("Case %d: %d\n", no, solve(n, k) + 1);
 	}
 
 	return 0;
