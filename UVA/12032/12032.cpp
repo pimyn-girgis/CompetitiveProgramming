547d43610b3f81269e18222ea46bf5d72abf19e3
pimyn-girgis <bemenboshra2001@gmail.com>
2023-06-20 13:36:12 +0300

    Added UVA 12032

---
+++ UVA/12032/12032.cpp
@@ -0,0 +1,17 @@
+/*
+**************************************************************
+* Author  : Bemen Girgis
+* Contact : bemen@girgis.org
+* Problem : 12032 UVA
+* Link    : https://onlinejudge.org/external/120/p12032.pdf
+**************************************************************
+* Approach: 
+**************************************************************
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	return 0;
+}
