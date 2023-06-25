acce94f51ea2a7ae17e0bb56c9f9b0baaee6cc71
pimyn-girgis <bemenboshra2001@gmail.com>
2023-06-25 03:14:04 +0300

    Added UVA 13142

---
+++ UVA/13142/13142.cpp
@@ -0,0 +1,17 @@
+/*
+**************************************************************
+* Author  : Bemen Girgis
+* Contact : bemen@girgis.org
+* Problem : 13142 UVA
+* Link    : https://onlinejudge.org/external/131/p13142.pdf
+**************************************************************
+* Approach: 
+**************************************************************
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	return 0;
+}
