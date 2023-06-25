221399d06d6af8dedeeaf95030463938017745bd
pimyn-girgis <bemenboshra2001@gmail.com>
2023-06-25 03:09:38 +0300

    Solved UVA 12032

--- UVA/12032/12032.cpp
+++ UVA/12032/12032.cpp
@@ -9,9 +9,49 @@
 **************************************************************
 */
 
+#include <algorithm>
 #include <bits/stdc++.h>
 using namespace std;
 
+int height[100005];
+int n;
+
+bool viable(int k) {
+	for (int i = 1; i <= n; ++i) {
+		const int diff = height[i] - height[i - 1];
+		if (diff == k) {
+			--k;
+		} else if (diff > k) {
+			return false;
+		}
+	}
+	return true;
+}
+
 int main() {
+	int tc; scanf("%d", &tc);
+
+	for (int caseNo = 1; caseNo <= tc; ++caseNo) {
+		scanf("%d", &n);
+
+		int j = 0;
+		for (int i = 1; i <= n; ++i) { // loop starts from 1 reserving height[0] = 0
+			scanf("%d", height + i);
+			j = max(j, height[i] + 1);
+		}
+
+		int i = 0; int ans = 1e9;
+		while (i < j) {
+			int mid = (i + j) / 2;
+			if (viable(mid)) {
+				j = mid;
+				ans = mid;
+			} else {
+				i = mid + 1;
+			}
+		}
+
+		printf("Case %d: %d\n", caseNo, ans);
+	}
 	return 0;
 }
